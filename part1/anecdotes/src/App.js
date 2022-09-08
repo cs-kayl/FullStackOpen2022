637bdb146bdbe5dcc3ad150b4aa55a8d42de08f5
John Thomas <kaylthomas607@gmail.com>
2022-09-08 15:03:08 -0400

    Exercise 1.13 anecdotes step2 completed

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -4,28 +4,43 @@ const Button = ({ text , onClick}) => {
   return <button onClick={onClick}>{text}</button>
 }
 
+const Anecdote = ({ anecdote, votes }) => {
+  return (
+    <>  
+      <p>{anecdote}</p>
+      <p>{"This anecdote has"} {votes} {"votes"}</p>
+    </>
+  )
+}
+
 const App = () => {
   const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
+    {anecdote:'If it hurts, do it more often.', votes: 0},
+    {anecdote:'Adding manpower to a late software project makes it later!',votes: 0},
+    {anecdote:'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',votes: 0},
+    {anecdote:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.', votes: 0},
+    {anecdote:'Premature optimization is the root of all evil.', votes:0},
+    {anecdote:'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.', votes:0},
+    {anecdote:'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.', votes:0}
   ]
   const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState(anecdotes)
 
-  const handleClick = (e) => {
-    console.log("EVENT TARGET", e.target) 
+  const handleNextAnecdote = () => {
     let max = anecdotes.length
     setSelected(Math.floor(Math.random()*max))
   }
 
+  const handleVote = () => {
+    votes[selected].votes += 1
+    setVotes({...votes})
+  }
+
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
-      <Button text={"next anecdote"} onClick={handleClick} />
+      <Anecdote anecdote={anecdotes[selected].anecdote} votes={votes[selected].votes} />
+      <Button text={"next anecdote"} onClick={handleNextAnecdote} />
+      <Button text={"vote"} onClick={handleVote} />
     </div>
   )
 }
