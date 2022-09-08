f0b1c59a910c59089afab121f0f0990e4f9f9ad7
John Thomas <kaylthomas607@gmail.com>
2022-09-08 09:04:25 -0400

    Exercise 1.8 unicafe step3 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,33 @@
 import { useState } from 'react' 
 
+const Statistics = ({ good, neutral, bad }) => {
+  const calcTotal = () => {
+    return good + neutral + bad
+  }
+
+  const calcAverage = () => {
+    let score = good - bad
+    let total = good + neutral + bad
+    return score/total
+  }
+
+  const calcPositive = () => {
+    let total = good + neutral + bad
+    return (good/total)*100
+  }
+
+  return (
+    <div>
+      <Feedback type={"good"} count={good} />
+      <Feedback type={"neutral"} count={neutral} />
+      <Feedback type={"bad"} count={bad} />
+      <p>{"all"} {calcTotal()}</p>
+      <p>{"average"} {calcAverage()}</p>
+      <p>{"positive"} {calcPositive()}{"%"}</p>
+    </div>
+  )
+}
+
 const Header = ({ header }) => {
   return <h1>{header}</h1>
 }
@@ -26,21 +54,6 @@ function App() {
     return () => callback(num + 1)
   }
 
-  const calcTotal = () => {
-    return goodCount + neutralCount + badCount
-  }
-
-  const calcAverage = () => {
-    let score = goodCount - badCount
-    let total = goodCount + neutralCount + badCount
-    return score/total
-  }
-
-  const calcPositive = () => {
-    let total = goodCount + neutralCount + badCount
-    return (goodCount/total)*100
-  }
-
   return (
     <>
       <Header header={"Give Feedback"}/>
@@ -48,12 +61,7 @@ function App() {
       <Button text={"neutral"} onClick={handleClick(neutralCount, setNeutralCount)} />
       <Button onClick={handleClick(badCount, setBadCount)} text={"bad"} />
       <Header header={"statistics"}/>
-      <Feedback type={"good"} count={goodCount} />
-      <Feedback type={"neutral"} count={neutralCount} />
-      <Feedback type={"bad"} count={badCount} />
-      <p>{"all"} {calcTotal()}</p>
-      <p>{"average"} {calcAverage()}</p>
-      <p>{"positive"} {calcPositive()}{"%"}</p>
+      <Statistics good={goodCount} neutral={neutralCount} bad={badCount} />
     </>
   );
 }
