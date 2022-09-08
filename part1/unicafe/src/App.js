3c45c4e1d9bc08f4954bd0442e42879c76a32601
John Thomas <kaylthomas607@gmail.com>
2022-09-07 20:11:25 -0400

    Exercise 1.6 unicafe step1 completed

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,7 +1,41 @@
+import { useState } from 'react' 
+
+const Header = ({ header }) => {
+  return <h1>{header}</h1>
+}
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const Feedback = ({ type, count }) => {
+  return <p>{type} {count} </p>
+}
+
+
 function App() {
-  
+  const [goodCount, setGoodCount] = useState(0)
+  const [neutralCount, setNeutralCount] = useState(0)
+  const [badCount, setBadCount] =  useState(0)
+
+  const handleClick = (num, callback) => {
+    return () => callback(num + 1)
+  }
+
   return (
     <>
+      <Header header={"Give Feedback"}/>
+      <Button onClick={handleClick(goodCount, setGoodCount)} text={"good"} />
+      <Button text={"neutral"} onClick={handleClick(neutralCount, setNeutralCount)} />
+      <Button onClick={handleClick(badCount, setBadCount)} text={"bad"} />
+      <Header header={"statistics"}/>
+      <Feedback type={"good"} count={goodCount} />
+      <Feedback type={"neutral"} count={neutralCount} />
+      <Feedback type={"bad"} count={badCount} />
     </>
   );
 }
